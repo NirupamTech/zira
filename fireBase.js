5a1581e89e5f29c5f76eaadca7c175899d9780ff
Nirupamreddy2006 <72504541+Nirupamreddy2006@users.noreply.github.com>
2020-12-23 11:05:38 +0530

    Add files via upload

---
+++ fireBase.js
@@ -0,0 +1,16 @@
+var app_fireBase = {};
+(function(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyArhbBcsJQxLhc7tLZYZ-Q9RoWal94z7ms",
+        authDomain: "ninfo-72795.firebaseapp.com",
+        projectId: "ninfo-72795",
+        storageBucket: "ninfo-72795.appspot.com",
+        messagingSenderId: "1007319722556",
+        appId: "1:1007319722556:web:131fd63815e7e3f979f139",
+        measurementId: "G-35SCRKN91B"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+      app_fireBase = firebase;
+})()
